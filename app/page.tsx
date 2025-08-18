c8b692d7f818fe3549fea9e6eee425c43223fae9
Fola <folaremidixon@gmail.com>
2025-08-18 16:25:44 +0100

    added dialog

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import { Volume2, VolumeX, Menu } from "lucide-react";
-import { motion, Variants } from "framer-motion";
+import { Volume2, VolumeX, Menu, X } from "lucide-react";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import Image from "next/image";
+import { Dialog, DialogContent, DialogOverlay, DialogTitle } from "@/components/ui/dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function LandingPage() {
   const [muted, setMuted] = useState(true);
+  const [open, setOpen] = useState(false);
 
   // Animation variants for cinematic text reveal
   const letterAnimation: Variants = {
@@ -135,10 +140,61 @@ export default function LandingPage() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 1.8 }}
           className="bg-[#B89C58] hover:bg-yellow-600 text-white px-6 py-3 rounded-md font-semibold"
+          onClick={() => setOpen(true)}
         >
           Follow the journey
         </motion.button>
       </div>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <AnimatePresence>
+          {open && (
+            <DialogContent className="sm:max-w-2xl p-0  overflow-hidden rounded-lg border-none">
+              {/* Required accessible title (can be hidden) */}
+              <DialogTitle>
+                <VisuallyHidden>Sign out confirmation</VisuallyHidden>
+              </DialogTitle>
+
+              <motion.div
+                initial={{ opacity: 0, y: 40, scale: 0.95 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                exit={{ opacity: 0, y: 40, scale: 0.95 }}
+                transition={{ duration: 0.25, ease: "easeOut" }}
+                className="p-6 sm:p-10"
+              >
+                <p className="text-gray-200 mb-4 text-sm">
+                  <span className="pl-20">The headlines may have faded, and the world may have moved on</span><br/>
+                  <span className="pl-10">But in Chibok, the mothers still carry faith like a seed in their hands.</span> <br/>
+                  <span className="pl-24">Send them a short message of hope as they continue to wait.</span>
+                </p>
+                <div className="mt-4 space-y-4 max-w-md mx-auto">
+                  <Input 
+                    type="text"
+                    placeholder="Enter your full name"
+                    className="w-full p-3 bg-gray-800 text-white placeholder-gray-500 rounded-md"
+                  />
+                  <Input 
+                    type="email"
+                    placeholder="Enter your email address"
+                    className="w-full p-3 bg-gray-800 text-white placeholder-gray-500 rounded-md"
+                  />
+                  <Button className="mt-4 w-full bg-[#B89C58] hover:bg-yellow-600 text-white">
+                    Join the journey
+                  </Button>
+                </div>
+
+                <div className="mx-auto">
+                  <Button onClick={() => setOpen(false)} className="mt-4 text-white text-center gap-2">
+                  <span>Close</span>
+                  <X className="w-4 h-4" />
+                </Button>
+                </div>
+                
+              </motion.div>
+            </DialogContent>
+          )}
+        </AnimatePresence>
+      </Dialog>
 
       {/* Bottom Text */}
       <motion.div
